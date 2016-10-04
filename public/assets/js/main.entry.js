2b5ee76983d9fecde67d1f50a4b728aa926c7a2b
Charlie Stanard <louis.stanard@gmail.com>
2016-10-03 21:44:22 -0600

    Refactoring todo done input to a checkbox instead of anchor

--- public/assets/js/main.entry.js
+++ public/assets/js/main.entry.js
@@ -55060,27 +55060,16 @@
 	    return app
 	        .directive('toggleComplete', function (Todo) {
 	            return {
-	                restrict: 'A',
 	                link: function (scope, elem, attrs) {
-	                    elem.bind('click', function (event) {
-	                        event.preventDefault();
-
-	                        $scope = scope;
-
-	                        var index = _.indexOf($scope.todos, _.find($scope.todos, { _id: $scope.todo._id }));
-
-	                        var todo = Todo.update(
-	                            { todo_id: $scope.todo._id },
+	                    scope.toggleComplete = function (todo) {
+	                        Todo.update(
+	                            { todo_id: todo._id },
 	                            {
-	                                todo: $scope.todo.text,
-	                                completed: !$scope.todo.completed
-	                            },
-	                            function () {
-	                                $scope.todos[index] = todo;
-	                                elem.parent().toggleClass('completed');
+	                                todo: todo.text,
+	                                completed: todo.completed
 	                            }
 	                        );
-	                    });
+	                    }
 	                }
 	            }
 	        });
